2738761b522d52c230b85930cf8bb95196ab9a91
su <abcc0785@gmail.com>
2022-02-22 11:24:00 +0900

    10773 풀이 수정

--- 10773.js
+++ 10773.js
@@ -1,10 +1,7 @@
-const fs = require('fs');
+const fs = require('fs'); 
+const input = fs.readFileSync("example.txt").toString().trim().split("\n");
 
-const input = fs.readFileSync('example.txt',"utf8").toString().split('\n'); //utf8이 없으니까 아무것도 출력 안되더라. 공식문서 보니 빈 버퍼만 나와서 나는 이렇게 해야했음. 
-
-//원하는 데이터만 받아오기
 const numsInput = input.map(el => Number(el)).slice(1);
-
 
 const result = [];
 
@@ -16,6 +13,11 @@ numsInput.forEach(num => {
   }
 })
 
-console.log(result.reduce((acc,cur) => acc + cur,0));
+let sum = 0;
+result.forEach(el => {
+    sum += el;
+})
+
+console.log(sum);
 
 
