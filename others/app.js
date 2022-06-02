e3e6eeef58a29b04b74b31503ff6799065eb42ef
su <abcc0785@gmail.com>
2022-06-02 21:51:02 +0900

    발표순서정하기

--- others/app.js
+++ others/app.js
@@ -1,41 +1,20 @@
-function barcode(len) {
-  //길이는 1부터 50까지 들어올 수 있다.
-  //가장 작은 수를 반환해야 한다.
-  //만약에 1을 입력받으면 당연히 1을 리턴할 것
-  if (len === 1) return "1";
+function factorial(n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
 
-  const isValid = (str) => {
-    //절반 길이
-    const half = Math.floor(str.length / 2);
-    //거꾸로 뒤집기
-    const reversed = str.split("").reverse().join("");
-
-    //절반 길이가 될 때까지 비교: 이비 비교한 부분은 다시 비교할 필요없다
-    for (let i = 1; i <= half; i++) {
-      if (reversed.slice(0, i) === reversed.slice(i, 2 * i)) return false;
-    }
-    return true;
-  };
-
-  //결과값을 담을 변수
-  let answer = "";
-  const aux = (answer) => {
-    for (let i = 1; i <= 3; i++) {
-      const next = answer + i;
-      if (isValid(next)) {
-        if (next.length === len) return next;
-        if (aux(next) !== null) return aux(next);
-      }
-    }
-    return null;
-  };
-
-  return aux(answer);
-  //1,2,3숫자를 붙여야 하는데 재귀적으로 붙여야 한다.
-  //붙이고 나서 만들 수 있는 바코드인지 검사한다.
-  //만들 수 없는 바코드라면 다른 숫자 붙여야 한다.
-  //1,2,3 하나씩 다 붙였는데도 만들 수 없다면 이전에 잘못된 숫자를 붙였다는 뜻이다. 이전 숫자 고쳐야 한다
-  //만들 수 있는 바코드라면
-  //len과 길이가 같다면 이때 그 바코드를 리턴한다
-  //아직 길이가 모자라다면 계속 같은 과정을 반복한다
+function orderOfPresentation(N, K) {
+  //정답 변수 선언
+  let answer = 0;
+  //주어진 배열 K를 순회한다
+  for (let i = 0; i < K.length; i++) {
+    //각 자리 숫자보다 작은 숫자가 몇 개 있는지 구한다 ...(1)
+    //이전의 숫자 제외하고 남은 숫자 중 작은 숫자 개수를 알아낸다.
+    const smaller = K.slice(i + 1).filter((el) => el < K[i]).length;
+    //남아있는 자리수를 순서 구분하여 나열하는 가지수 구한다...(2)
+    const remain = factorial(K.slice(i + 1).length);
+    //1과 2를 곱한 값을 answer에 누적하며 더한다
+    answer += smaller * remain;
+  }
+  return answer;
 }
