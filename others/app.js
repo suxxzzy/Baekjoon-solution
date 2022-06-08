9ff3bce8d67d396e49c1b729d8510c2f4a40e48c
su <abcc0785@gmail.com>
2022-06-08 09:49:35 +0900

    타일링 문제, tabulation

--- others/app.js
+++ others/app.js
@@ -1,14 +1,26 @@
-function bubbelsort(arr) {
-  //배열의 크기만큼 반복
-  for (let i = 0; i < arr.length; i++) {
-    let swap = 0;
-    for (let j = 0; j < arr.length - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-        swap++;
-      }
-    }
-    if (swap === 0) break;
+let tiling = function (n) {
+  //sol1: 현재 결과는 이전의 두 결과의 합과 같음을 이용한다. tabulation
+  //1,2,3,5,8,13...
+  const arr = [1, 2];
+  for (let i = 2; i <= n; i++) {
+    arr[i] = arr[i - 2] + arr[i - 1];
   }
-  return arr;
-}
+  return arr[n - 1];
+  // //sol2 가로의 개수에 따라 결과값을 얻어낸다
+  // const fac = function (n) {
+  //   const arr = [1];
+  //   for (let i = 1; i <= n; i++) {
+  //     arr[i] = arr[i - 1] * i;
+  //   }
+  //   return arr[n];
+  // };
+  // const h = Math.floor(n / 2);
+  // let answer = 0;
+  // for (let i = 0; i <= h; i++) {
+  //   const v = n - 2 * i;
+  //   answer += fac(v + i) / fac(i) / fac(v);
+  // }
+  // return answer;
+};
+
+console.log(tiling(4));
