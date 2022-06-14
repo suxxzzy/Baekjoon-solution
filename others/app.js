b1ceed19f251cad0e82e427a7e6bd744bc329f71
su <abcc0785@gmail.com>
2022-06-14 11:24:38 +0900

    power

--- others/app.js
+++ others/app.js
@@ -1,34 +1,40 @@
-function largestProductOfThree(arr) {
-  //항상 배열의 길이가 3 이상인 경우만 생각한다.
-  //배열의 길이가 3인 경우는 손쉽게 바로 곱을 구하면 끝이다.
-  if (arr.length === 3) {
-    return arr.reduce((acc, cur) => acc * cur);
-  }
-  //배열의 길이가 3을 넘는 경우
-  //일단 오름차순으로 배열 정렬해보자.
-  arr.sort((a, b) => a - b);
-  const last = arr.length - 1;
+//tab: 테스트 시 문법 오류 있다고 함
+function powerTab(base, exponent) {
+  const cache = [1, base % 94906249];
+  //예외 처리
+  //지수가 1이하인 경우
+  if (exponent <= 1) return cache[exponent];
 
-  //배열에서 가장 큰 수가 0인 경우: 0
-  if (arr[last] === 0) return 0;
-
-  //배열이 모두 음수인 경우: 가장 마지막 요소가 0보다 작은 경우다. 제일 마지막 요소 3개를 곱한다.
-  //배열에서 가장 작은 수가 0 이상인 경우
-  //배열이 0, 양수 포함하는 경우
-  //배열이 양수만 포함하는 경우
-  if (arr[last] < 0 || arr[0] >= 0) {
-    return arr[last - 2] * arr[last - 1] * arr[last];
+  //그 외의 경우: 주어진 지수를 반으로 나눈다. 예를 들어 6인 경우 f(3) *f(3) 5인 경우 f(3) *f(2)
+  //이미 계산한 값은 다시 계산하지 않도록, 저장해둘 곳이 필요하다. tabulation 사용
+  //이미 계산되어 있는 값일 경우
+  //계산되지 않은 값일 경우
+  for (let i = 2; i <= exponent; i++) {
+    const half = Math.floor(i / 2);
+    cache[i] = (cache[half] * cache[i - half]) % 94906249;
   }
-  //배열이  음수, 0, 양수 모두 포함하는 경우
-  //음수 2개 이상
-  const minus = arr.filter((el) => el < 0);
-  if (minus.length >= 2) {
-    const cand1 = arr[last - 2] * arr[last - 1] * arr[last];
-    const cand2 = arr[0] * arr[1] * arr[last];
-    return Math.max(cand2, cand1);
-  }
-  //음수 1개
-  if (minus.length === 1) {
-    return arr[last - 2] * arr[last - 1] * arr[last];
-  }
+  return cache[exponent];
 }
+
+function powerLog(base, exponent) {
+  const cache = [1, base % 94906249];
+
+  const aux = function (exponent) {
+    //예외 처리
+    //지수가 1이하인 경우
+    if (exponent <= 1) return cache[exponent];
+
+    //지수가 2 이상인 경우: 반절
+    if (cache[exponent] !== undefined) return cache[exponent];
+
+    const half = Math.floor(exponent / 2);
+    cache[exponent] = (aux(half) * aux(exponent - half)) % 94906249;
+    return cache[exponent];
+  };
+
+  return aux(exponent);
+}
+
+let output = powerLog(3, 40);
+output = powerTab(3, 40);
+console.log(output);
