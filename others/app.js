fad972a21d704a0926122f44fb824e59ca9d5bbe
su <abcc0785@gmail.com>
2022-06-15 11:48:42 +0900

    binary search

--- others/app.js
+++ others/app.js
@@ -1,40 +1,30 @@
-//tab: 테스트 시 문법 오류 있다고 함
-function powerTab(base, exponent) {
-  const cache = [1, base % 94906249];
-  //예외 처리
-  //지수가 1이하인 경우
-  if (exponent <= 1) return cache[exponent];
-
-  //그 외의 경우: 주어진 지수를 반으로 나눈다. 예를 들어 6인 경우 f(3) *f(3) 5인 경우 f(3) *f(2)
-  //이미 계산한 값은 다시 계산하지 않도록, 저장해둘 곳이 필요하다. tabulation 사용
-  //이미 계산되어 있는 값일 경우
-  //계산되지 않은 값일 경우
-  for (let i = 2; i <= exponent; i++) {
-    const half = Math.floor(i / 2);
-    cache[i] = (cache[half] * cache[i - half]) % 94906249;
+const binarySearch = function (arr, target) {
+  //특이한 경우: 배열의 길이가 0이거나 1인 경우
+  if (arr.length === 0 || (arr.length === 1 && arr[0] !== target)) return -1;
+  if (arr.length === 1 && arr[0] === target) return 0;
+  //주어진 배열을 반으로 나눈다.
+  let start = 0;
+  let end = arr.length - 1;
+  let half = Math.floor(arr.length / 2);
+  //중간값인 경우: 바로 half 리턴
+  //중간값보다 큰 경우: 탐색을 시작할 인덱스를 half + 1로 조정 후, 같은 과정을 반복
+  //중간값보다 작은 경우: 탐색을 종료할 인덱스를 half - 1로 조정 후 같은 과정을 반복
+  //언제 멈출까? 시작값과 중간값이 동일한 경우 멈춘다
+  while (start <= half) {
+    if (arr[half] === target) return half;
+    if (arr[half] < target) {
+      start = half + 1;
+    }
+    if (arr[half] > target) {
+      end = half - 1;
+    }
+    half = Math.floor((start + end) / 2);
   }
-  return cache[exponent];
-}
+  return -1;
+};
 
-function powerLog(base, exponent) {
-  const cache = [1, base % 94906249];
+let output = binarySearch([0, 1, 2, 3, 4, 5, 6], 2);
+console.log(output); // --> 2
 
-  const aux = function (exponent) {
-    //예외 처리
-    //지수가 1이하인 경우
-    if (exponent <= 1) return cache[exponent];
-
-    //지수가 2 이상인 경우: 반절
-    if (cache[exponent] !== undefined) return cache[exponent];
-
-    const half = Math.floor(exponent / 2);
-    cache[exponent] = (aux(half) * aux(exponent - half)) % 94906249;
-    return cache[exponent];
-  };
-
-  return aux(exponent);
-}
-
-let output = powerLog(3, 40);
-output = powerTab(3, 40);
-console.log(output);
+output = binarySearch([4, 5, 6, 9], 100);
+console.log(output); // --> -1
